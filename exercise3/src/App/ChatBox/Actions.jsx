8e2a92f051adf92d226d5b22fa2ff2f4213afabc
Ali <ali19961402@gmail.com>
2023-11-11 17:16:50 +0330

    first-commit

---
+++ exercise3/src/App/ChatBox/Actions.jsx
@@ -0,0 +1,10 @@
+import './actions.css';
+function Actions(){
+    return(
+        <div className="actions">
+            <button>ارسال</button>
+            <input type="text" placeholder="...پیام خود را اینجا بنویسید"/>
+        </div>
+     );   
+}
+export default Actions;
