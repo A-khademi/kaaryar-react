85a41b72e6ccc37635ce94716e4a59fe70ced6e9
Ali <ali19961402@gmail.com>
2023-11-18 01:02:00 +0330

    list-2

--- exercise4/src/List/index.jsx
+++ exercise4/src/List/index.jsx
@@ -1,4 +1,21 @@
-function List({orders}){
+import "./list.css"
 
+function List({ orders }) {
+  const orderSell = orders.filter((order) => {
+    return order.type === "sell";
+  });
+  const orderBuy = orders.filter((order) => {
+    return order.type === "buy";
+  });
+  return (
+    <div>
+      {orderSell.map((order) => (
+        <div className="list">
+        <p>{orderSell.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
+
 export default List;
