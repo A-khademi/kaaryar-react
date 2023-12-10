d62eb00aaa90ecc09c65ba825cd712cf780dfa15
Ali <ali19961402@gmail.com>
2023-12-10 21:23:49 +0330

    json

--- exercise5/src/Items/index.jsx
+++ exercise5/src/Items/index.jsx
@@ -1,21 +1,19 @@
 import "./style.css";
-
+import "../works.json";
 function Items() {
   return (
     <div className="items">
       <div className="item">
-        <div className="works">
-          <span>
-            {" "}
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil,
-            debitis! Rem mollitia non quia, ad, magni quaerat quod unde eligendi
-            voluptates ut optio quae quasi nemo reprehenderit necessitatibus
-            doloribus quas.
-          </span>
-        </div>
-        <div className="button">
-          <span>X</span>
-        </div>
+        <ul>
+          <li>
+            <div className="works">
+              <span>job</span>
+            </div>
+            <div className="button">
+              <span>X</span>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
